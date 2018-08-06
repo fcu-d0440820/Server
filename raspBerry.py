e96bc609e1ba0eb1e4e457daf573668db9169c67
fcu-d0440820 <d0440820@fcu.edu.tw>
2018-08-06 16:01:18 +0800

    Update raspBerry.py

--- raspBerry.py
+++ raspBerry.py
@@ -6,7 +6,35 @@ This is a temporary script file.
 """
 
 import socket   
+import RPi.GPIO as gpio
+import time
 
+gpio.setmode(gpio.BOARD)
+gpio.setwarnings(False)
+ 
+pin = [29, 31, 33, 35]
+for i in range(4):
+    gpio.setup(pin[i], gpio.OUT)
+ 
+forward_sq = ['0011', '1001', '1100', '0110']
+reverse_sq = ['0110', '1100', '1001', '0011']
+ 
+def forward(steps, delay):
+    for i in range(steps):
+        for step in forward_sq:
+            set_motor(step)
+            time.sleep(delay)
+ 
+def reverse(steps, delay):
+    for i in range(steps):
+        for step in reverse_sq:
+            set_motor(step)
+            time.sleep(delay)
+ 
+def set_motor(step):
+    for i in range(4):
+        gpio.output(pin[i], step[i] == '1')
+        
 HOST='localhost'  
 PORT=6666
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)  
@@ -22,6 +50,10 @@ while True:
 
     if data =="exit" :
         break
+        
+    elif data =="feed" :
+        set_motor('0000')
+        forward(241, 0.005)
         
     cmd = "好開勳".encode("utf-8")
     conn.send(cmd)
